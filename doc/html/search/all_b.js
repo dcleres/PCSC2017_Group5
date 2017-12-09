07f1c9b79d3b56fd79a2fff0a86210aa7f66ae60
dcleres <david.cleres@epfl.ch>
2017-12-09 23:26:53 +0100

    doxyfile is up-to-date

--- doc/html/search/all_b.js
+++ doc/html/search/all_b.js
@@ -1,15 +1,15 @@
 var searchData=
 [
   ['set_5fstyle',['set_style',['../class_gnuplot.html#accdd7b69237ead4109c74e1e440c185f',1,'Gnuplot']]],
-  ['set_5fx',['set_x',['../class_point.html#a0e06062c8608e039f214cd0815634aa2',1,'Point']]],
+  ['set_5fx',['set_x',['../class_point.html#a7a9b270860e9198583176ad5c42f9187',1,'Point']]],
   ['set_5fxlabel',['set_xlabel',['../class_gnuplot.html#ac9b0c04d47e375eb82f50437eda5e46e',1,'Gnuplot']]],
-  ['set_5fy',['set_y',['../class_point.html#a140e7315cdb4c66f1230df7e7b92ffa5',1,'Point']]],
+  ['set_5fy',['set_y',['../class_point.html#a40f61acc21fc1416c2cc1034ee2a928a',1,'Point']]],
   ['set_5fylabel',['set_ylabel',['../class_gnuplot.html#af85dd1d368699914112460285ead0fde',1,'Gnuplot']]],
   ['show',['show',['../class_read_file.html#a5efc41b900510ae038dafc23a2563300',1,'ReadFile']]],
   ['simulation_2em',['simulation.m',['../simulation_8m.html',1,'']]],
   ['solve',['solve',['../class_lagrange.html#a9040a01112a83bbefb0f1bef1d7983bb',1,'Lagrange::solve()'],['../class_polynomial.html#ae67730df0a2e45a87c9cb244fe03ad6b',1,'Polynomial::solve()']]],
-  ['solve_5flagrange',['solve_lagrange',['../class_piece_wise___continue___polynomial.html#a7fbf288a1307b1853e537d02d4e57da7',1,'PieceWise_Continue_Polynomial']]],
-  ['solve_5fleast_5fsquare',['solve_least_square',['../class_piece_wise___continue___polynomial.html#ae4e6583489b97d59121f04514521a901',1,'PieceWise_Continue_Polynomial']]],
+  ['solve_5flagrange',['solve_lagrange',['../class_piece_wise_continue_polynomial.html#a19731122369c27f68802687e7d64aaf5',1,'PieceWiseContinuePolynomial']]],
+  ['solve_5fleast_5fsquare',['solve_least_square',['../class_piece_wise_continue_polynomial.html#a39966b5bfa4d0ca686eb4fa191dbfa7c',1,'PieceWiseContinuePolynomial']]],
   ['stringify',['STRINGIFY',['../_c_make_c_compiler_id_8c.html#a43e1cad902b6477bec893cb6430bd6c8',1,'STRINGIFY():&#160;CMakeCCompilerId.c'],['../_c_make_c_x_x_compiler_id_8cpp.html#a43e1cad902b6477bec893cb6430bd6c8',1,'STRINGIFY():&#160;CMakeCXXCompilerId.cpp']]],
   ['stringify_5fhelper',['STRINGIFY_HELPER',['../_c_make_c_compiler_id_8c.html#a2ae9b72bb13abaabfcf2ee0ba7d3fa1d',1,'STRINGIFY_HELPER():&#160;CMakeCCompilerId.c'],['../_c_make_c_x_x_compiler_id_8cpp.html#a2ae9b72bb13abaabfcf2ee0ba7d3fa1d',1,'STRINGIFY_HELPER():&#160;CMakeCXXCompilerId.cpp']]],
   ['stringtok',['stringtok',['../gnuplot__i_8cpp.html#ad684ebbe0be47cf7fd23ce12dce37ddb',1,'gnuplot_i.cpp']]]
--- doc/html/search/functions_5.js
+++ doc/html/search/functions_5.js
@@ -1,8 +1,9 @@
 var searchData=
 [
+  ['piecewisecontinuepolynomial',['PieceWiseContinuePolynomial',['../class_piece_wise_continue_polynomial.html#a1f3e40054d55cacc9079084683e259e7',1,'PieceWiseContinuePolynomial']]],
   ['plot_5fequation',['plot_equation',['../class_gnuplot.html#a55e6430f1329bf8a66f155dcacb9d112',1,'Gnuplot']]],
   ['plot_5fslope',['plot_slope',['../class_gnuplot.html#a80c9d9e6bc3e64db073d9d39d6ec5d5f',1,'Gnuplot']]],
   ['plot_5fx',['plot_x',['../class_gnuplot.html#ae3b7c28efb53f636431b9655085906be',1,'Gnuplot']]],
   ['plot_5fxy',['plot_xy',['../class_gnuplot.html#a1e817a58ef3e40ceaeb128c7e6437e8a',1,'Gnuplot']]],
-  ['point',['Point',['../class_point.html#acfded4fb511fce4b9c7f4704a310d628',1,'Point::Point(int taille)'],['../class_point.html#ac5bfcc2d69bc85e707d6a656bce77bfe',1,'Point::Point(vector&lt; double &gt;x, vector&lt; double &gt;y)']]]
+  ['point',['Point',['../class_point.html#a44d490126797f9e88de376fb6814b089',1,'Point::Point(size_t const &amp;taille)'],['../class_point.html#a64da1fdafc9131972f1458ef9bd5de30',1,'Point::Point(vector&lt; double &gt; const &amp;x, vector&lt; double &gt; const &amp;y)']]]
 ];
