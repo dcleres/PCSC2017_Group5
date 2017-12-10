372e101b035a428c5d694eeff6fec7df4016a21c
pcsc <pcsc@pcsc.epfl.ch>
2017-12-09 19:33:51 -0500

    test commit because master buggs

--- doc/html/search/classes_4.js
+++ doc/html/search/classes_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['piecewise_5fcontinue_5fpolynomial',['PieceWise_Continue_Polynomial',['../class_piece_wise___continue___polynomial.html',1,'']]],
+  ['piecewisecontinuepolynomial',['PieceWiseContinuePolynomial',['../class_piece_wise_continue_polynomial.html',1,'']]],
   ['point',['Point',['../class_point.html',1,'']]],
   ['point_5ft',['point_t',['../structpoint__t.html',1,'']]],
   ['polynomial',['Polynomial',['../class_polynomial.html',1,'']]]
--- doc/html/search/files_5.js
+++ doc/html/search/files_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['piecewise_5fcontinue_5fpolynomial_2ecpp',['PieceWise_Continue_Polynomial.cpp',['../_piece_wise___continue___polynomial_8cpp.html',1,'']]],
-  ['piecewise_5fcontinue_5fpolynomial_2eh',['PieceWise_Continue_Polynomial.h',['../_piece_wise___continue___polynomial_8h.html',1,'']]],
+  ['piecewisecontinuepolynomial_2ecpp',['PieceWiseContinuePolynomial.cpp',['../_piece_wise_continue_polynomial_8cpp.html',1,'']]],
+  ['piecewisecontinuepolynomial_2eh',['PieceWiseContinuePolynomial.h',['../_piece_wise_continue_polynomial_8h.html',1,'']]],
   ['plots_2em',['plots.m',['../plots_8m.html',1,'']]],
   ['point_2ecpp',['Point.cpp',['../_point_8cpp.html',1,'']]],
   ['point_2eh',['Point.h',['../_point_8h.html',1,'']]],
